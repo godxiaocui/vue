28e88fc2c4010ac64cd93db74af066e776a8d377
zh.cui <zh.cui@trip.com>
2023-11-15 06:50:01 +0800

    1

--- 06/src-interview/src/App.vue
+++ 06/src-interview/src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h5-wrapper">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "h5-wrapper",
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+<style lang="less" scoped>
+.h5-wrapper {
+  .content {
+    margin-bottom: 51px;
+  }
+  .tabbar {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    display: flex;
+    background: #fff;
+    border-top: 1px solid #e4e4e4;
+    a {
+      flex: 1;
+      text-decoration: none;
+      font-size: 14px;
+      color: #333;
+      -webkit-tap-highlight-color: transparent;
+      &.router-link-active {
+        color: #fa0;
+      }
+    }
+  }
 }
 </style>
--- 06/src-interview/src/router/index.js
+++ 06/src-interview/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
-import VueRouter from "vue-router";
+import VueRouter  from 'vue-router'
 import Layout from '@/views/Layout'
-Vue.use(VueRouter)
 
+
+Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     {
       path:'/',
-      compoment: Layout, 
+      component: Layout, 
     }
   ]
 })
 
 export default router
+
